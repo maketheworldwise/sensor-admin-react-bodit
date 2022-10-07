1b231a2f8c2481aa7290f15c3e38b89c254c77f9
miogy <jjangrl87@gmail.com>
2022-10-07 20:22:54 +0900

    UI:graph 레이아웃

--- src/Pages/Graph/Graph.js
+++ src/Pages/Graph/Graph.js
@@ -1,11 +1,15 @@
 import { useEffect, useState } from 'react';
 import { getThingspeak } from './Api';
-
+import { BsCalendarMonth } from 'react-icons/bs';
 import styles from './Graph.module.scss';
 import TempChart from './Temp';
 
 function Graph() {
   const [data, setData] = useState({});
+  const current = new Date();
+  const date = `${current.getDate()}/${
+    current.getMonth() + 1
+  }/${current.getFullYear()}`;
 
   useEffect(() => {
     getThingspeak().then(json => setData(json));
@@ -13,7 +17,20 @@ function Graph() {
 
   return (
     <div className={styles.graph_container}>
-      <TempChart TempData={data} />
+      <div className={styles.graph_title}>
+        <p>{date}</p>
+        <p>
+          <BsCalendarMonth />
+        </p>
+      </div>
+
+      <ul className={styles.graph_wrap}>
+        <li>
+          <TempChart TempData={data} />
+        </li>
+        <li>{/* 컴포넌트 */}</li>
+        <li>{/* 컴포넌트 */}</li>
+      </ul>
     </div>
   );
 }
