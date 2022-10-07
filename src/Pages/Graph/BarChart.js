42798ba8d056be22d0b0952da69334c54ca6165e
Miog Yang <99234582+miogy@users.noreply.github.com>
2022-10-07 22:23:44 +0900

    [Feature] Temp데이터연결 (#7)
    Feature:Temp그래프
    - 데이터 연결
    
    UI:graph 레이아웃
    - 그래픽페이지 레이아웃
    
    Issue: #4

---
+++ src/Pages/Graph/BarChart.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Chart as ChartJs, LineElement } from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
+
+ChartJs.register(LineElement);
+
+function BarChart() {
+  const data = {
+    labels: ['Red', 'Blue', 'Yellow'],
+    datasets: [
+      {
+        label: 'My First Dataset',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 205, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(201, 203, 207, 0.2)',
+        ],
+        borderColor: [
+          'rgb(255, 99, 132)',
+          'rgb(255, 159, 64)',
+          'rgb(255, 205, 86)',
+          'rgb(75, 192, 192)',
+          'rgb(54, 162, 235)',
+          'rgb(153, 102, 255)',
+          'rgb(201, 203, 207)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  let options = {
+    maintainAspectRatio: false,
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+    legend: {
+      labels: {
+        fontSize: 26,
+      },
+    },
+  };
+
+  return (
+    <div>
+      <Line data={data} options={options} height={400} />
+    </div>
+  );
+}
+export default BarChart;
--- src/Pages/Graph/Graph.js
+++ src/Pages/Graph/Graph.js
@@ -1,16 +1,38 @@
 import { useEffect, useState } from 'react';
 import { getThingspeak } from './Api';
-
+import { BsCalendarMonth } from 'react-icons/bs';
 import styles from './Graph.module.scss';
+import TempChart from './Temp';
 
 function Graph() {
   const [data, setData] = useState({});
+  const current = new Date();
+  const date = `${current.getDate()}/${
+    current.getMonth() + 1
+  }/${current.getFullYear()}`;
 
   useEffect(() => {
     getThingspeak().then(json => setData(json));
   }, []);
 
-  return <div className={styles.graph_container}>Graph</div>;
+  return (
+    <div className={styles.graph_container}>
+      <div className={styles.graph_title}>
+        <p>{date}</p>
+        <p>
+          <BsCalendarMonth />
+        </p>
+      </div>
+
+      <ul className={styles.graph_wrap}>
+        <li>
+          <TempChart TempData={data} />
+        </li>
+        <li>{/* 컴포넌트 */}</li>
+        <li>{/* 컴포넌트 */}</li>
+      </ul>
+    </div>
+  );
 }
 
 export default Graph;
