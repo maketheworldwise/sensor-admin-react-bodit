23f1b788af879a93ca02f7320ff547fe3d3ea729
이고운 <kowoon@igoun-ui-MacBookAir.local>
2022-10-07 17:38:33 +0900

    Add: calendar 추가

--- src/Pages/Calendar/Calendar.js
+++ src/Pages/Calendar/Calendar.js
@@ -1,7 +1,37 @@
 import styles from './Calendar.module.scss';
+import Calendar from 'react-calendar';
+import Moment from 'react-moment';
+import { useState, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { getThingspeak } from '../Graph/Api';
+import 'react-calendar/dist/Calendar.css';
 
-function Calendar() {
-  return <div className={styles.calendar_container}>Calendar</div>;
+function CalendarPage() {
+  const { CHANNEL_ID } = useParams();
+  const [value, onChange] = useState(new Date());
+  const [data, setData] = useState([]);
+
+  const navigate = useNavigate();
+
+  const onClickDay = () => {
+    // navigate('/');
+  };
+
+  const fetchdata = () => {};
+
+  useEffect(() => {
+    getThingspeak(CHANNEL_ID).then(json => setData(json.data));
+    console.log(data);
+  }, []);
+
+  return (
+    <div className={styles.calendar_container}>
+      Calendar
+      <Calendar onClickDay={onClickDay} onChange={onChange} value={value} />
+      {/* <div> {moment(value).format('YYYY년 MM월 DD일')}</div> */}
+      {data}
+    </div>
+  );
 }
 
-export default Calendar;
+export default CalendarPage;
